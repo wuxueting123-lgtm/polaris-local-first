478ca1bdcbfa0c5734b9b2c14fe58cdc1a245c52
wuxueting123-lgtm <wuxueting.123@163.com>
2026-08-02 20:47:05 +0800

    Update MainActivity.java

--- android/app/src/main/java/com/alyssa/polaris/MainActivity.java
+++ android/app/src/main/java/com/alyssa/polaris/MainActivity.java
@@ -2,6 +2,8 @@ package com.alyssa.polaris;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends BridgeActivity {
     @Override
@@ -10,5 +12,14 @@ public class MainActivity extends BridgeActivity {
         registerPlugin(LocalDataSqlitePlugin.class);
         registerPlugin(NativeProviderHttpPlugin.class);
         super.onCreate(savedInstanceState);
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+            getWindow().getDecorView().getRootView(),
+            (v, insets) -> {
+                int bottom = insets.getInsets(WindowInsetsCompat.Type.navigationBars()).bottom;
+                v.setPadding(0, 0, 0, bottom);
+                return insets;
+            }
+        );
     }
 }
